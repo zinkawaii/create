64b3db2ebf00995f80fcd1936bb5d966f97ea667
KazariEX <1364035137@qq.com>
2024-12-07 19:32:43 +0800

    init: 初始化仓库

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: [
+        "./src/index.ts"
+    ],
+    format: [
+        "esm"
+    ],
+    clean: true,
+    shims: true
+});
