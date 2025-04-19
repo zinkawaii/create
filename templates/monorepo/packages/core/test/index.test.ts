efd861332f14486fc31843db6437464a6ee93c3b
KazariEX <1364035137@qq.com>
2025-04-19 18:17:39 +0800

    feat: 添加 monorepo 模板

---
+++ templates/monorepo/packages/core/test/index.test.ts
@@ -0,0 +1,5 @@
+import { expect, it } from "vitest";
+
+it("test", () => {
+    expect(1 + 1).toEqual(2);
+});
---
+++ templates/monorepo/packages/core/tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+    entry: [
+        "./src/index.ts"
+    ],
+    format: [
+        "esm"
+    ],
+    clean: true,
+    dts: true
+});
