bb1d6906d61ae77bb427af774b09079761aa2241
KazariEX <1364035137@qq.com>
2025-04-21 16:10:01 +0800

    refactor: 移除默认 `format` 构建配置

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,9 +4,6 @@ export default defineConfig({
     entry: [
         "./src/index.ts"
     ],
-    format: [
-        "esm"
-    ],
     clean: true,
     outputOptions: {
         banner: "#!/usr/bin/env node"
